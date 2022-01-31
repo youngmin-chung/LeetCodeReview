29d8b8f7cfd934841566f13f05ed9a331263ef15
Youngmin Chung <dev.youngmin@gmail.com>
2022-01-31 10:43:53 -0500

    Time: 82 ms (64.73%), Space: 42.6 MB (8.89%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let res = [];
+    for(var i = 0; i < nums2.length; i++){
+        if(nums1.includes(nums2[i])){
+            res.push(nums2[i]);
+            nums1.splice(nums1.indexOf(nums2[i]), 1);
+        }
+    }
+    
+    return res;
+};
