d52904cb2c762da2c0732dff84a9dab77b9c15d7
Youngmin Chung <dev.youngmin@gmail.com>
2022-02-15 21:57:34 -0500

    Time: 63 ms (94.02%), Space: 41.4 MB (33.14%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+    var k = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] != val){
+            // partition
+            
+            nums[k++] = nums[i];
+            
+        }
+    }
+   
+    return k;
+};
